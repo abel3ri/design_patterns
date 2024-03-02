49724a87cbce74293e39c9b6abd7f2d75f30fd38
abel3ri <abelmerete@yahoo.com>
2024-03-02 09:52:14 +0300

    Add abstract button factory with concrete implemented classes

--- AbstractFactory.java
+++ AbstractFactory.java
@@ -6,3 +6,31 @@ interface Button{
     void render();
     void onPressed();
 }
+
+
+class AndroidButton implements Button{
+
+    @Override
+    public void render(){
+        System.out.println("Android button rendered.");
+    }
+
+    @Override
+    public void onPressed(){
+        System.out.println("Android button pressed.");
+    }
+}
+
+class IOSButton implements Button{
+
+    @Override
+    public void render(){
+        System.out.println("IOS button rendered.");
+    }
+
+    @Override
+    public void onPressed(){
+        System.out.println("IOS button pressed.");
+    }
+}
+
